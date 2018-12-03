17e994d0e44eb68a5db8e6321c24c32bb68caba9
BradleyNing <bradley.ning@outlook.com>
2018-12-03 11:16:53 +0000

    modify

--- Network/python-network/Network-topdown/udp_server.py
+++ Network/python-network/Network-topdown/udp_server.py
@@ -1,7 +1,7 @@
 from socket import *
 
 serverPort = 9999
-serverSocket = socket('', serverPort)
+serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 
 print("The server is ready to receive")
@@ -11,5 +11,5 @@ while True:
 	modifiedMsg = msg.decode().upper()
 	serverSocket.sendto(modifiedMsg.encode(), clientAddr)
 
-	
+
 
