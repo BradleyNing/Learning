a5358494b9aa0ae671dc5aecd5ec823d11027c08
BradleyNing <bradley.ning@outlook.com>
2018-12-03 10:41:03 +0000

    add network-toptown first

---
+++ Network/python-network/Network-topdown/udp_client.py
@@ -0,0 +1,12 @@
+from socket import *
+
+serverName = '192.168.1.113'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCKET_DGRAM)
+
+msg = input('Input lowercase sentense: ')
+clientSocket.sendto(msg.encode(), (serverName, serverPort))
+
+modfiedMsg, serverAddress = clientSocket.recvfrom(2048)
+print(modfiedMsg.decode())
+clientSocket.close()
